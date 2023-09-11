894ce1d4140034a7098e8cf26442f7948e10289c
Jihun <wlgjs06061@naver.com>
2023-09-11 22:40:40 +0900

    Feat: config

---
+++ shopping-mall_api/src/main/java/com/jhkr1/shopping/mall_api/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.jhkr1.shopping.mall_api.config;
+
+import com.jhkr1.shopping.mall_api.security.jwt.exception.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthenticationManagerConfig authenticationManagerConfig;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        return http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .csrf().disable()
+                .cors()
+                .and()
+                .apply(authenticationManagerConfig)
+                .and()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .mvcMatchers(GET, "/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                .mvcMatchers(POST, "/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                .anyRequest().hasAnyRole()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
+                .build();
+    }
+
+    // <<Advanced>> Security Cors로 변경 시도
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // config.setAllowCredentials(true); // 이거 빼면 된다
+        // https://gareen.tistory.com/66
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("GET","POST","DELETE","PATCH","OPTION","PUT"));
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- shopping-mall_api/src/main/java/com/jhkr1/shopping/mall_api/controller/HelloApiController.java
+++ shopping-mall_api/src/main/java/com/jhkr1/shopping/mall_api/controller/HelloApiController.java
@@ -4,6 +4,7 @@ import com.jhkr1.shopping.mall_api.security.jwt.util.JwtTokenizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class HelloApiController {
     private final JwtTokenizer jwtTokenizer;
 
     @GetMapping("/hello")
-    public String hello(@RequestBody("Authorization") String token) {
+    public String hello(@RequestHeader("Authorization") String token) {
         Long userIdFromToken = jwtTokenizer.getUserIdFromToken(token);
         return "hello " + userIdFromToken;
     }
