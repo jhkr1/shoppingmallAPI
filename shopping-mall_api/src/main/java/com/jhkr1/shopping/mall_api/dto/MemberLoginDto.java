b6cd21bcf3a6ea27a88f84fdc688e975a0bd3ebf
Jihun <wlgjs06061@naver.com>
2023-09-11 22:05:35 +0900

    Feat: dto 추가

---
+++ shopping-mall_api/src/main/java/com/jhkr1/shopping/mall_api/dto/MemberLoginDto.java
@@ -0,0 +1,24 @@
+package com.jhkr1.shopping.mall_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginDto {
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+    private String email;
+
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+    private String password;
+
+}
---
+++ shopping-mall_api/src/main/java/com/jhkr1/shopping/mall_api/dto/MemberLoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.jhkr1.shopping.mall_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+
+    private Long memberId;
+    private String nickname;
+}
